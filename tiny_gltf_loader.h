252aa73e57d2458f9835375cdb25a32843a6b8c0
gauravalgo <gaurav61989@gmail.com>
2023-05-14 12:17:06 +0530

    first commit

---
+++ tiny_gltf_loader.h
@@ -0,0 +1,17 @@
+#ifndef TINY_GLTF_LOADER_H
+#define TINY_GLTF_LOADER_H
+
+#include <string>
+#include <vector>
+#include <map>
+
+namespace tinygltf {
+
+  #define TINYGLTF_MODE_POINTS (0)
+  #define TINYGLTF_MODE_LINE (1)
+  #define TINYGLTF_MODE_LINE_LOOP (2)
+  #define TINYGLTF_MODE_TRIANGLES (4)
+  #define TINYGLTF_MODE_TRIANGLE_STRIP (5)
+  #define TINYGLTF_MODE_TRIANGLE_FAN (6)
+  
+}
