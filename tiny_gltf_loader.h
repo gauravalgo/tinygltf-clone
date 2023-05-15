8e5b63fc9ce5d7f3b442c77a3ea0aa3619d0ce99
gauravalgo <gaurav61989@gmail.com>
2023-05-15 13:19:59 +0530

    adding array for picojson

--- tiny_gltf_loader.h
+++ tiny_gltf_loader.h
@@ -124,4 +124,429 @@ namespace tinygltf {
 
   };
 
+  typedef struct {
+      std::string name;
+      std::vector<unsigned char> data;
+  }Buffer;
+
+  typedef struct {
+    std::string generator;
+    std::string version;
+    std::string profile_api;
+    std::string profile_version;
+    bool premultipliedAlpha;  
+  }Asset;
+
+  class Scene {
+    public :
+    Scene();
+    ~Scene();
+
+    std::map<std::string,Accessor> accessors;
+    std::map<std::string,Buffer> buffers;
+    std::map<std::string,BufferView> bufferViews;
+    std::map<std::string,Mesh> meshes;
+    std::map<std::string,Node> nodes;
+    std::map<std::string,std::vector<std::string> > scenes; // list of nodes
+
+    std::string defaltScene;
+    Asset asset;    
+  };
+
+  class TinyGLTFLoader{
+      public:
+      TinyGLTFLoader(){};
+      ~TinyGLTFLoader(){};
+      //Loads GLTF assest from a file.
+      //Returns false and set error string to 'err' if there's and parsing error .
+
+      bool LoadFromFile(Scene &scene,std::string &err,
+                        const std::string &filename);
+  };
+
+}//namespace tinygltf
+
+#ifdef TINYGLTF_LOADER_IMPLEMENTATION
+#include <sstream>
+#include <fstream>
+#include <cassert>
+
+#include "picojson.h"
+#include "stb_image.h"
+
+#ifdef _WIN32
+#include<Windows.h>
+#else
+#include<wordexp.h>
+#endif
+
+using namespace tinygltf;
+
+namespace {
+  bool FileExists(const std::string &abs_filepath){
+    bool ret;
+    FILE *fp= fopen(abs_filename.c_str(),"rb");
+    if(fp){
+      ret = true;
+      fclose(fp);
+    }else{
+      ret = false;
+    }
+
+    return ret;
+  }
+
+
+  std::string ExpandFilePath(const std::string &filepath){
+#ifdef _WIN32
+    DWORD len = ExpandEnviromentStringsA(filepath.c_str(),NULL,0);
+    char *str = new char[len];
+    ExpandEnviromentStringsA(filepath.c_str(),str,len);
+    std::string s(str);
+    delete[] str;
+    return s;
+  }
+#else
+#if TARGET_OS_IPHONE || TARGET_IPHONE_SIMULATOR
+  //NO EXPANSION
+  std::string s = filepath;
+#else
+  std::string s;
+  wordexp_t p;
+  if (filepath.empty()){
+    return "";
+  }
+  //char **w
+  int ret = wordexp(filepath.c_str(),&p,0);
+  if(ret)
+  {
+    //err
+    s= filepath;
+    return s;
+  }
+  // use first element only
+  if(p.we_wordv)
+  {
+    s=std::string(p.we_wordv[0]);
+    wordfree(&p);
+  }
+  else{
+    s= filepath;
+  }
+#endif
+return s;
+#endif
 }
+std::string JoinPath(const std::string &path0 , const std::string &path1){
+
+  if(path0.empty())
+  {
+    return path1;
+  }
+  else{
+    //check '/'
+    char lastChar = *path0.rbegin();
+    if(lastChar!= '/'){
+      return path0 + std::string("/")+path1;
+    }
+    else{
+      return path0 + path1;
+    }
+  }
+}
+
+std::string FindFile(const std::vector<std::string> &paths,
+                      const std::string &filepath){
+                        for(size_t i=0 ;i<paths.size();i++)
+                        {
+      std::string absPath = ExpandFilePath(JoinPath(paths[i],filepath));
+      if(FileExists(absPath)){
+        return absPath;
+      }
+    }
+          return std::string();
+}
+std::string GetBaseDir(const std::string & FileName)
+{
+  if(filepath.find_last_of("/\\")! = std::string::npos)
+    return FileN.ame.substr(0,filepath.find_last_of("/\\"));
+    return "";
+}
+//std::string base64_encode(std::string const &s);
+
+/*
+base64.cpp and base64.h
+
+
+*/
+inline std::string get_base64_chars(){
+  static std::string base64_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                    "abcdefghijklmnopqrstuvwxyz"
+                                    "0123456789+/";
+  return base64_chars;
+}
+
+inline std::string base64_encode(std::string const &data)
+{
+  int counter = 0;
+  uint32_t bit_stream = 0;
+  const std::string base64_chars = get_base64_chars();
+  std::string encoded;
+  int offset = 0;
+  for (unsigned char c : data)
+  {
+    auto num_val = static_cast<unsigned int >(c);
+    offset = 16 - counter % 3 * 8;
+    bit_stream += num_val << offset;
+    if(offset == 16){
+    encoded += base64_chars.at(bit_stream >> 18 & 0x3f);
+    }
+    if(offset == 8)
+    {
+      encoded += base64_chars.at(bit_stream >> 12 & 0x3f);
+    }
+    if(offset == 0 && counter!=3)
+    {
+      encoded += base64_chars.at(bit_stream >> 6 & 0x3f);
+      encoded += base64_chars.at(bit_stream & 0x3f);
+      bit_stream = 0;
+    }
+    counter++;
+  }
+  if(offset == 16)
+  {
+    encoded+= base64_chars.at(bit_stream>>12 & 0x3f);
+    encoded+= "==";
+  }
+  if(offset == 8)
+  {
+    encoded += base64_chars.at(bit_stream >> 6 & 0x3f);
+    encoded+= '=';
+  }
+  return encoded;
+
+}
+
+inline std::string base64_decode(std::string const &data) {
+  int counter = 0;
+  uint32_t bit_stream = 0;
+  std::string decoded;
+  int offset = 0;
+  const std::string base64_chars = get_base64_chars();
+  for(unsigned char c : data)
+  {
+    auto num_val = base64_chars.find(c);
+    if(num_val != std::string::npos) {
+      offset = 18 -counter % 4 * 6;
+      bit_stream+= num_val << offset;
+    
+    if(offset == 12)
+    {
+      decoded+= static_cast<char>(bit_stream >> 16 & 0xff);
+    }
+    if(offset == 6)
+    {
+      decoded+= static_cast<char>(bit_stream >> 8 & 0xff);
+    }
+    if(offset ==0 && counter!=4){
+      decoded+= static_cast<char>(bit_stream & 0ff);
+      bit_stream =0 ;
+    }
+  }
+  else if(c!= '='){
+    return std::string();
+  }
+  counter++;
+}
+return decoded;
+}
+
+bool LoadExternalFile(std::vector<unsigned char> &out,std::string &err,
+                      const std::string &filename, const std::string &basedir,
+                        size_t reqBytes,bool checkSize){
+out.clear();
+std::vector<std::string> paths;
+paths.push_back(basedir);
+paths.push_back(".");
+
+std::string filepath = FindFile(paths,filename);
+if(filepath.empty())                        
+{
+  err += "File open error :" + filepath;
+  return false;
+}
+std::ifstream f(filepath.c_str(),std::ifstream::binary);
+if(!f){
+  err+= "File open error :"+ filepath;
+  return false;
+}
+f.seekg(0,f.end());
+size_t sz = f.tellg();
+std::vector<unsigned char> buf(sz);
+
+f.seekg(0,f.beg);
+f.read(reinterpret_cast<char *>(&buf.at(0)),sz);
+f.close();
+
+if(checkSize){
+  if(reqBytes == sz)
+  {
+    out.swap(buf);
+    return true;
+  }
+  else{
+    std::stringstream ss;
+    ss<< "File size mismatch :" << filepath << ", requeatedBytes "<< reqBytes 
+      <<", but got "<< sz << std::endl;
+      err+= ss.str();
+      return false;
+  }
+
+}
+out.swap(buf);
+return true;
+}
+
+bool IsDataURI(const std::string &in){
+  std::string header = "data:application/octect-stream;base64,";
+  if(in.find(header) == 0)
+  {
+    return true;
+  }
+  return false;
+}
+
+bool DecodeDataURI(std::vector<unsigned char> &out , const std::string &in,
+                  size_t reqBytes,bool checkSize){
+                    std::string header = "data:application/octect-stream;base64";
+                    if(in.find(header) == 0) {
+                      std::string data = 
+                      base64_decode(in.substr(header.size())); ///cut mime string.
+                      if(checkSize){
+                        if(data.size()!= reqBytes){
+                          return false;
+                        }
+                        out.resize(reqBytes);
+                      }
+                      else{
+                        out.resize(data.size());
+                      }
+                      std::copy(data.begin(),data.end(),out.begin());
+                      return true;
+                    }
+                    return false;
+                  }
+          
+bool ParseBooleanProperty(bool &ret , std::string &err, 
+                        const picojson::object &o,
+                        const std::string &property ,bool required){
+
+picojson::object::const_iterator it = o.find(property);
+if(it == o.end()){
+  if(required){
+    err+= "'" + property + "'property is missing.\n";
+  }
+  return false;
+}
+
+if(!it->second.is<bool>()){
+  if(required){
+    err+= "'" + property + "'property is not a bool type.\n";
+  }
+  return false;
+}
+ret = it->second.get<bool>();
+return true;
+}
+
+bool ParseNumberProperty(double &ret,std::string &err,
+                const picojson::object &o, const std::string &property,
+                bool required){
+                  picojson::object::const_iterator it = o.find(property);
+                  if(it == o.end()) {
+                    if(required) {
+                      err += "'" + property + "' property id missing .\n";
+                    }
+                    return false;
+                  }
+                  ret = it->second.get<double>();
+                  return true;
+                }
+
+bool ParseNumberArrayProperty(std::vector<double> &ret ,std::string &err,
+            const picojson::object &o,
+            const std::string &property,bool required)
+            {
+picojson::object::const_iterator it = o.find(property);
+if(it == o.end()){
+  if(required){
+    err += "'" + property + "'property is missing.\n";
+  }
+  return false;
+}
+if(!it->second.is<picojson::array>()){
+  if(required){
+    err += "'" + property + "'property is not a number.\n";
+  }
+  return false;
+}
+
+ret.clear();
+const picojson::array &arr = it->second.get<picojson::array>();
+for(size_t i =0;i<arr.size();i++){
+  if(!arr[i].is<double>()) {
+    if(required){
+      err += "'" + property + "'property is not a number.\n";
+    }
+    return false;
+  }
+  ret.push_back(arr[i].get<double>());
+}
+return true;
+}
+
+bool ParseStringProperty(std::string &ret , std::string &err,
+const picojson::object &o,const std::string &property,
+                    bool required) {
+picojson::object::const_iterator it = o.find(property);
+if(it == o.end()){
+  if(required){
+    err+= "'" + property + "'property is missing.\n";
+  }
+  return false;
+}
+if(!it->second.is<std::string>()){
+  if(required){
+    err += "'" + property + "'property is not a string type.\n";
+  }
+}
+return it->second->get<std::string>();
+return true;
+}
+
+bool ParseStringArrayProperty(std::vector<std::string> &ret,std::string &err,
+                                const picojson::object &o,
+                                const std::string& property,bool required ){
+      picojson::object::const_iterator it = o.find(property);
+      if(it == o.end()){
+        if(required){
+          err += "'" + property + "' property is missing .\n";
+        }
+        return false;
+      }
+      if(!it->second.is<picojson::array>()){
+        if(required){
+          err += "'" + property +"'property is not an array.\n";
+        }
+        return false;
+      }
+      ret.clear();
+      const picojson::array &arr = it->second.get<picojson::array>();
+      for(size_t i=0)
+                                }
+                    
+                    
+                    
+                    
+
+
