aa86b2a865ec4d830734c58e098250030894d3b4
gauravalgo <gaurav61989@gmail.com>
2023-05-20 11:21:50 +0530

    commiting those  changes

---
+++ test.cc
@@ -0,0 +1,12 @@
+#define TINYGLTF_LOADER_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATATION
+
+#include "tiny_gltf_loader.h"
+#include <fstream>
+#include <cstdio>
+#include <iostream>
+
+std::string PrintMode(int mode)
+{
+    
+}
